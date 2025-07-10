55efa70748673d4c64c432d54d938e4c606cfe5b
anastasia.test <nastyona957@yandex.ru>
2025-07-10 14:46:11 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,19 @@
 public class Main {
     public static void main(String[] args) {
 
+        int balance = 100; // Начальный баланс клиента
+        int depositAmount = 1100; // Сумма пополнения
 
+        int bonus = 0;
+        bonus = depositAmount / 100; // Начисление бонуса за каждые 100 рублей пополнения
+
+
+        int finalBalance = balance + depositAmount + bonus;
+
+        // Вывод результатов
+        System.out.println("Сумма пополнения: " + depositAmount + " рублей");
+        System.out.println("Бонус: " + bonus + " рублей");
+        System.out.println("Итоговый баланс: " + finalBalance + " рублей");
 
         }
     }
