af77abc5c310055598258b02c095d4337f43f288
anastasia.test <nastyona957@yandex.ru>
2025-07-11 15:13:22 +0300

    formatting the code

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
         int balance = 100; // Начальный баланс клиента
         int depositAmount = 1100; // Сумма пополнения
-
         int bonus;
         bonus = depositAmount / 100; // Начисление бонуса за каждые 100 рублей пополнения
-
-
         int finalBalance = balance + depositAmount + bonus;
-
         // Вывод результатов
         System.out.println("Сумма пополнения: " + depositAmount + " рублей");
         System.out.println("Бонус: " + bonus + " рублей");
         System.out.println("Итоговый баланс: " + finalBalance + " рублей");
-
     }
 }
