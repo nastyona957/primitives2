45a26c1c109462248a0cbd4a2374e98da8659078
anastasia.test <nastyona957@yandex.ru>
2025-07-12 08:25:07 +0300

    code changes

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int balance = 100; // Начальный баланс клиента
+        int initialBalance = 100; // Начальный счет клиента
         int depositAmount = 1100; // Сумма пополнения
-        int bonus;
-        bonus = depositAmount / 100; // Начисление бонуса за каждые 100 рублей пополнения
-        int finalBalance = balance + depositAmount + bonus;
-        // Вывод результатов
-        System.out.println("Сумма пополнения: " + depositAmount + " рублей");
-        System.out.println("Бонус: " + bonus + " рублей");
-        System.out.println("Итоговый баланс: " + finalBalance + " рублей");
+        int bonusThreshold = 1000; // Порог пополнения для начисления бонуса
+        int bonusPer100 = 1; // Бонус за каждые 100 рублей пополнения
+        int bonus = 0;
+
+        // Проверяем, превысила ли сумма пополнения порог
+        if(depositAmount > bonusThreshold) {
+            bonus = (depositAmount / 100) * bonusPer100;
+        }
+        // Рассчитываем итоговый счет
+        int finalBalance = initialBalance + depositAmount + bonus;
+
+        System.out.println("Сумма пополнения: " + depositAmount + " руб.");
+        System.out.println("Бонус: " + bonus + " руб.");
+        System.out.println("Итоговый счет: " + finalBalance + " руб.");
     }
 }
